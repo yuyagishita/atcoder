be83012fc4be9c73b053f3c4204b0bc9c8e16f97
yuyagishita <yu.yagishita1995@gmail.com>
2024-03-21 00:34:03 +0900

    コーディングテストに強くなるためまた頑張る

--- rust-compete-example/abc189/src/bin/b.rs
+++ rust-compete-example/abc189/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,  // アイテムの総数
+        x: u64,    // 特定の閾値または制限
+        vp: [(u64, u64); n],  // 各アイテムとそのパーセンテージのリスト
+    }
+
+    let mut total_alcohol = 0;
+    for (i, (v, p)) in vp.into_iter().enumerate() {
+        total_alcohol += v * p;
+        if total_alcohol > x * 100 {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+
+    println!("{}", -1);
 }
+
--- rust-compete-example/abc189/src/bin/c.rs
+++ rust-compete-example/abc189/src/bin/c.rs
@@ -1,3 +1,22 @@
+use proconio::*;
+
 fn main() {
-    todo!();
+    input!{
+        n: usize,
+        mut a: [u32; n],
+    }
+    a.push(0);
+    let mut ans: u32 = 0;
+    let mut stack: Vec<(u32, u32)> = vec![];
+    for a in a{
+        let mut w: u32 = 0;
+        while stack.last().map_or(false, |p: &(u32, u32)| p.0 >= a){
+            let (a, b) = stack.pop().unwrap();
+            w += b;
+            ans = ans.max(a * w);
+        }
+        stack.push((a, w + 1));
+    }
+    println!("{:?}", ans);
 }
+
